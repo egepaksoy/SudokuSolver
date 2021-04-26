0880ce261f2ff3a7dfea889b3a975a21a7de7e0f
Ege Paksoy <egepaskoy@gmail.com>
2021-04-26 08:47:45 +0300

    Search algoritmasi kaldi

--- main.py
+++ main.py
@@ -32,22 +32,26 @@ def divide(the_table):
   row = []
   column = []
 
+
+  # divide rows
   x = 0
   while x < len(the_table):
     row.append([])
     for row_block in the_table[x]:
-      for number in row_block:
-        row[x].append(number)
+      for r in row_block:
+        row[x].append(r)
     x += 1
 
+
+  # divide columns
   buyuk = 0
   x = 0
   while buyuk < 3:
     kucuk = 0
     while kucuk < 3:
       column.append([])
-      for row in the_table:
-        column[x].append(row[buyuk][kucuk])
+      for r in the_table:
+        column[x].append(r[buyuk][kucuk])
       kucuk += 1
       x += 1
     buyuk += 1
@@ -55,6 +59,14 @@ def divide(the_table):
 
   return row, column
 
+def search(divided, num):
+  for x in divided[0]:
+    if num not in x:
+      print(x)
+  print("xxxxxxxxxxxxxxxx")
+  for y in divided[1]:
+    if num not in y:
+      print(y)
 
 table = [
   [[0, 0, 0], [9, 0, 0], [1, 0, 0]],
@@ -72,4 +84,7 @@ table = [
 
 
 best_nums = get_number(table)
-divided = divide(table)
+bolunmus = divide(table)
+
+
+search(bolunmus, best_nums[0])
