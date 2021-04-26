1dc3e866a3654d05ac80ed733a1bef65a47db12a
Ege Paksoy <egepaskoy@gmail.com>
2021-04-26 02:31:01 +0000

    get_number ibaresi en büyük değerleri sırayla döndürüyor

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+def get_number(the_table):
+  search_nums = {}
+  find_number = []
+  best = 0
+
+  for row in the_table:
+    for mini_row in row:
+      for number in mini_row:
+        if number != 0:
+          if number not in search_nums:
+            search_nums[number] = 1
+          else:
+            search_nums[number] += 1
+
+
+  for i in range(len(search_nums)):
+    best = 0
+    best_num = 0
+
+    for number in search_nums:
+      if search_nums[number] > best:
+        best = search_nums[number]
+        best_num = number
+
+    search_nums[best_num] = 0
+    find_number.append(best_num)
+
+
+  return find_number
+
+
 table = [
   [[0, 0, 0], [9, 0, 0], [1, 0, 0]],
   [[0, 1, 9], [7, 0, 0], [6, 0, 3]],
@@ -12,20 +43,8 @@ table = [
   [[1, 0, 0], [0, 0, 6], [3, 7, 9]]
 ]
 
-search_nums = {}
-find_number = 0
-best = 0
 
-for row in table:
-  for mini_row in row:
-    for number in mini_row:
-      if number != 0:
-        if number not in search_nums:
-          search_nums[number] = 1
-        else:
-          search_nums[number] += 1
+best_nums = get_number(table)
 
-for number in search_nums:
-  if search_nums[number] > best:
-    best = search_nums[number]
-    find_number = number
+for num in best_nums:
+  # Search in the table for true position
